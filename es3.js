ff9f2f55aa2f32be98ce7f1b3259064591cc2c8c
RogerP <rog.p@gmx.com>
2011-10-12 12:26:11 -0700

    started emulating typed es3

--- es3.js
+++ es3.js
@@ -22,48 +22,182 @@
 
 //This does not conform *exactly* with ES3 - only where practical
 //e.g. we do not *practically* need a [[Call]] property
-//Please do not send push requests just for the sake of conforming to the specification
+//Please do not send pull requests just for the sake of conforming to the specification
 
 function CreateGlobal(node) {
-	return; //Don't need this for alpha stage
-	
-	function CreateObject() {
-		return {
-			"[[Class]]": "Object",
-			
-		};
-	}
-	function CreateFunction(args) {
-		var fun = {
-			"[[Class]]": "Function",
-			"[[Prototype]]": Function.prototype,
-			
-			length: {
-				value: args >>> 0,
+
+	var internal = {
+		"Object": {
+			prototype: {
+				"[[Class]]": "Object",
+				"[[Prototype]]": null,
 				
 				"[[DontEnum]]": true,
 				"[[DontDelete]]": true,
-				"[[ReadOnly]]": true
-			}
-		};
-		
-		return fun;
-	}
-	
-	var internal = {
-		"Object": {
+				"[[ReadOnly]]": true,
+				
+				//constructor: Object,
+				toString: CreateFunction("String"),
+				toLocaleString: CreateFunction("String"),
+				valueOf: CreateFunction("*"), //TODO: wrong?
+				hasOwnProperty: CreateFunction("Boolean", "String"),
+				isPrototypeOf: CreateFunction("Boolean", "Object"),
+				propertyIsEnumerable: CreateFunction("Boolean", "String")
+			},
+			
+			//"[[Prototype]]": Function.prototype
 		},
 		"Function": {
 			prototype: {
 				"[[Class]]": "Function",
-				"[[Prototype]]": internal.Object.prototype,
+				//"[[Prototype]]": Object.prototype,
+				
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true,
 			
-				constructor: internal.Function,
-				toString: {
-				}
+				//constructor: Function,
+				toString: CreateFunction("String"),
+				apply: CreateFunction("*", "Object", "Array"),
+				call: CreateFunction("*", "Object", "...*")
+			},
+			
+			"[[Prototype]]": this.prototype,
+			
+			//constructor: Function,
+			length: {
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true
 			}
 		}
 	};
+
+	//Create extendable base ES3-compatible object
+	//"Every built-in prototype object has the Object prototype object, which is
+	// the initial value of the expression Object.prototype (15.2.3.1), as the 
+	// value of its internal [[Prototype]] property, except the Object prototype
+	// object itself."
+	function CreateObject() {
+		var obj = {
+			"[[Class]]": "Object",
+			"[[Prototype]]": Object.prototype,
+			
+			"[[DontEnum]]": true,
+			
+			prototype: {
+				"[[Class]]": "Object",
+				"[[Prototype]]": null,
+				
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true,
+				
+				constructor: this.Object,
+				toString: CreateFunction("String"),
+				toLocaleString: CreateFunction("String"),
+				valueOf: CreateFunction("*"),
+				hasOwnProperty: CreateFunction("Boolean", "String"),
+				isPrototypeOf: CreateFunction("Boolean", "Object"),
+				propertyIsEnumerable: CreateFunction("Boolean", "String")
+			},
+			
+			constructor: {
+				"[[Prototype]]": this.Function.prototype
+			}
+		};
+		
+		return obj;
+	}
+	var objPro = CreateObject().prototype;
+	
+	//Create extendable base ES3-compatible function
+	//"Every built-in function and every built-in constructor has the Function 
+	// prototype object, which is the initial value of the expression 
+	// Function.prototype (15.3.2.1), as the value of its internal [[Prototype]] 
+	// property."
+	function CreateFunction() {
+		var returnType = arguments[0];
+		arguments = Array.prototype.slice.call(arguments, 1);
+		
+		var fun = {
+			"[[Class]]": "Function",
+			"[[Type]]": "Function", //Non-standard
+			"[[Prototype]]": Function.prototype,
+			
+			"[[DontEnum]]": true,
+			
+			type: jsdef.FUNCTION,
+			returntype: returnType,
+			
+			length: {
+				value: arguments.length >>> 0,
+				
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true,
+				
+				"[[Type]]": "Number"
+			}
+		};
+		
+		fun.constructor = {
+			prototype: {
+				"[[Class]]": "Function",
+				"[[Type]]": "Function",
+				"[[Prototype]]": objPro,
+				
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true,
+			
+				toString: fn_toString,
+				apply: fn_apply,
+				call: fn_call
+			},
+			
+			length: {
+				"[[DontEnum]]": true,
+				"[[DontDelete]]": true,
+				"[[ReadOnly]]": true,
+				
+				"[[Type]]": "Number"
+			}
+		};
+		fun.prototype = {
+			constructor: fun,
+			
+			"[[Type]]": "Object" //Non-standard
+		};
+		fun.constructor["[[Prototype]]"] = fun.constructor.prototype;
+		fun.constructor.constructor = fun.constructor;
+		
+		for (var i=0, len=arguments.length; i<len; i++) {
+			fun[i] = {
+				"[[Type]]": arguments[i] //Non-standard
+			};
+		}
+		
+		return fun;
+	}
+	var fn_toString = CreateFunction("String"),
+		fn_apply = CreateFunction("*", "Object", "Array"),
+		fn_call = CreateFunction("*", "Object", "...*");
+	
+	//Extend object
+	function extend(a, b) {
+		var c = {};
+		
+		for (var i in a) {
+			c[i] = a[i];
+		}
+		
+		for (var j in b) {
+			c[j] = b[j];
+		}
+		
+		return c;
+	}
 
 	//ES3 15.1.1: Value Properties of the Global Object
 	node.Variables.push({
@@ -75,7 +209,7 @@ function CreateGlobal(node) {
 	});
 	node.Variables.push({
 		identifier: "Infinity",
-		value: 1/0,
+		value: 1 / 0,
 	
 		"[[DontEnum]]": true,
 		"[[DontDelete]]": true
@@ -88,26 +222,60 @@ function CreateGlobal(node) {
 		"[[DontDelete]]": true
 	});
 	//ES3 15.1.2: Function Properties of the Global Object
-	node.Variables.push({
-		identifier: "parseInt",
-		value: parseInt,
-		arity: 1,
-	
-		"[[DontEnum]]": true
-	});
-	node.Variables.push({
+	node.Variables.push(extend(CreateFunction("*", "*"), {
 		identifier: "eval",
-		value: eval,
-		arity: 1,
+		value: eval
+	}));
+	node.Variables.push(extend(CreateFunction("Number", "String", "Number"), {
+		identifier: "parseInt",
+		value: parseInt
+	}));
+	node.Variables.push(extend(CreateFunction("Number", "String"), {
+		identifier: "parseFloat",
+		value: parseFloat
+	}));
+	node.Variables.push(extend(CreateFunction("Boolean", "Number"), {
+		identifier: "isNaN",
+		value: isNaN
+	}));
+	node.Variables.push(extend(CreateFunction("Boolean", "Number"), {
+		identifier: "isFinite",
+		value: isFinite
+	}));
+	//ES3 15.1.3: URI Handling Function Properties
+	node.Variables.push(extend(CreateFunction("String", "String"), {
+		identifier: "decodeURI",
+		value: decodeURI
+	}));
+	node.Variables.push(extend(CreateFunction("String", "String"), {
+		identifier: "decodeURIComponent",
+		value: decodeURIComponent
+	}));
+	node.Variables.push(extend(CreateFunction("String", "String"), {
+		identifier: "encodeURI",
+		value: encodeURI
+	}));
+	node.Variables.push(extend(CreateFunction("String", "String"), {
+		identifier: "encodeURIComponent",
+		value: encodeURIComponent
+	}));
 	
-		"[[DontEnum]]": true
+	//ES3 15.2: Object Objects
+	var _Object = extend(CreateFunction("Object", "*"), {
+		identifier: "Object",
+		value: {},
+	
+		"[[Prototype]]": internal.Function.prototype,
+		"[[Class]]": "Object",
+		"[[DontDelete]]": false
 	});
+	node.Variables.push(_Object);
 
 	node.Variables.push({
 		identifier: "String",
-		value: undefined,
+		value: "",
 	
-		"[[Prototype]]": internal.Function._prototype,
+		"[[Prototype]]": internal.Function.prototype,
 		"[[Class]]": "String",
 		"[[ReadOnly]]": false,
 		"[[DontEnum]]": false,
